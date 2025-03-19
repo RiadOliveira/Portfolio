21c47de485c40dc3c409bf0bfd91712b4ca87e87
Ríad Oliveira <riad.oliveira@hotmail.com>
2025-03-19 02:20:23 -0300

    <Main/> rounded updated

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 export function Main() {
   return (
-    <main className="w-[calc(100%-1rem)] flex-1 mb-2 mx-auto p-4 rounded">
+    <main className="w-[calc(100%-1rem)] flex-1 mb-2 mx-auto p-4 rounded-md">
       <h1>Hello World</h1>
       <p>Contet Test</p>
     </main>
