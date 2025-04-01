586081f9ed3ef7ac42bd5eca077be85480c89b15
Ríad Oliveira <riad.oliveira@hotmail.com>
2025-03-31 21:48:07 -0300

    Default Theme set to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { POPPINS, SPACE_GROTESK } from 'constants/fonts';
 
 export default function RootLayout({ children }: DefaultProps) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={`${POPPINS} ${SPACE_GROTESK} antialiased`}>{children}</body>
     </html>
   );
--- src/components/main/ThemeButton.tsx
+++ src/components/main/ThemeButton.tsx
@@ -5,7 +5,7 @@ import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md';
 import { toggleTheme } from 'utils/toggleTheme';
 
 export function ThemeButton() {
-  const [darkTheme, setDarkTheme] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(true);
 
   function handleThemeToggle() {
     toggleTheme();
@@ -16,7 +16,7 @@ export function ThemeButton() {
     <button
       type="button"
       onClick={handleThemeToggle}
-      className="size-8.5 p-1 rounded-full text-light hover:bg-dark/90 dark:hover:bg-light/90 dark:hover:text-dark active:bg-dark/90 dark:active:bg-light/90 dark:active:text-dark duration-300"
+      className="size-8.5 p-1 rounded-full text-light hover:bg-dark/85 dark:hover:bg-light/85 dark:hover:text-dark active:bg-dark/85 dark:active:bg-light/85 dark:active:text-dark duration-300"
     >
       {darkTheme ? (
         <MdOutlineLightMode className="size-full" />
