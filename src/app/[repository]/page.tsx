c13516688ef4f2be2d8034253957c47f54b698ae
Ríad Oliveira <riad.oliveira@hotmail.com>
2025-04-07 19:32:15 -0300

    RepositoryView Footer buttons

--- src/app/[repository]/page.tsx
+++ src/app/[repository]/page.tsx
@@ -5,6 +5,9 @@ import { MainTitle } from 'components/utilities/main-title';
 import { USER } from 'constants/user';
 import { RepositoryData } from 'types/RepositoryData';
 import { TechnologyBadge } from 'components/utilities/technology-badge';
+import { IoReader } from 'react-icons/io5';
+import { MdArrowBackIosNew } from 'react-icons/md';
+import { FaGithub } from 'react-icons/fa';
 
 type Params = {
   params: Promise<{ repository: string }>;
@@ -28,28 +31,47 @@ export default async function RepositoryView({ params }: Params) {
     <>
       <MainTitle>{repository}</MainTitle>
 
-      <section className="relative pt-2 flex-1">
-        <p className="text-base text-justify mb-4">{EXAMPLE_REPOSITORY.description}</p>
+      <section className="relative pt-2 flex-1 flex flex-col justify-between">
+        <div>
+          <p className="text-base text-justify mb-4.5">{EXAMPLE_REPOSITORY.description}</p>
 
-        <ImageContainer
-          className="bg-emerald-600/60 border-t-1 border-x-1 border-light/40 shadow-md rounded-t-md overflow-hidden aspect-video w-full max-w-4xl lg:w-4xl"
-          src={`https://user-images.githubusercontent.com/${USER.id}/${EXAMPLE_REPOSITORY.displayData.coverImage}`}
-          alt={EXAMPLE_REPOSITORY.name}
-          sizes={{ definitions: [{ threshold: 'LG', value: '56rem' }], defaultValue: '100%' }}
-        />
+          <ImageContainer
+            className="bg-emerald-500/60 border-t-1 border-x-1 border-light/40 shadow-md rounded-t-md overflow-hidden aspect-video w-full max-w-4xl lg:w-4xl"
+            src={`https://user-images.githubusercontent.com/${USER.id}/${EXAMPLE_REPOSITORY.displayData.coverImage}`}
+            alt={EXAMPLE_REPOSITORY.name}
+            sizes={{ definitions: [{ threshold: 'LG', value: '56rem' }], defaultValue: '100%' }}
+          />
 
-        <div className="bg-emerald-600/60 shadow-md flex flex-wrap border-x-1 border-b-1 border-light/40 rounded-b-md py-3 gap-2 justify-center">
-          <TechnologyBadge technology="React" />
-          <TechnologyBadge technology="NodeJs" />
-          <TechnologyBadge technology="Expo" />
-          <TechnologyBadge technology="C" />
-          <TechnologyBadge technology="C++" />
+          <div className="bg-emerald-500/60 shadow-md flex flex-wrap border-x-1 border-b-1 border-light/40 rounded-b-md py-3 gap-2 justify-center">
+            <TechnologyBadge technology="React" />
+            <TechnologyBadge technology="NodeJs" />
+            <TechnologyBadge technology="Expo" />
+            <TechnologyBadge technology="C" />
+            <TechnologyBadge technology="C++" />
+          </div>
         </div>
 
-        <div className="absolute bottom-0 w-full flex justify-between">
-          <Link href="/" className="flex justify-center py-2.5 gap-3 w-5/12 rounded bg-dark">
-            Go Back
+        <div className="flex justify-end gap-6 sm:justify-center sm:pt-10">
+          <Link
+            href="/"
+            className="size-10 p-2 rounded-md hover:bg-light/30 active:bg-light/30 transition-colors duration-400"
+          >
+            <MdArrowBackIosNew className="size-6 -ml-0.5" />
           </Link>
+
+          <a
+            href={`https://github.com/RiadOliveira/${EXAMPLE_REPOSITORY.name}`}
+            target="_blank"
+            className="bg-violet-700 h-10 px-3 flex items-center justify-center gap-2 rounded-md hover:scale-105 active:scale-105 transition-normal duration-200"
+          >
+            <IoReader size={24} />
+            <span className="text-base font-semibold">Preview</span>
+          </a>
+
+          <button className="bg-gray-800 h-10 px-3 flex items-center justify-center gap-2 rounded-md hover:scale-105 active:scale-105 transition-normal duration-200">
+            <FaGithub size={24} />
+            <span className="text-base font-semibold">Github</span>
+          </button>
         </div>
       </section>
     </>
