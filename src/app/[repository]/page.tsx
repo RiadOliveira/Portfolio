31c567d6b0b700cba48358fdc3f26816bdc35e29
Ríad Oliveira <riad.oliveira@hotmail.com>
2025-04-29 11:26:51 -0300

    Design corrections

--- src/app/[repository]/page.tsx
+++ src/app/[repository]/page.tsx
@@ -32,7 +32,7 @@ export default async function RepositoryView({ params }: RepositoryViewParams) {
     <>
       <MainTitle>{repository}</MainTitle>
 
-      <section className="relative flex flex-1 flex-col justify-between pt-2">
+      <section className="no-scrollbar relative mt-2 flex flex-1 flex-col justify-between overflow-y-scroll">
         <div>
           <p className="mb-4.5 text-justify text-base">{EXAMPLE_REPOSITORY.description}</p>
 
@@ -52,7 +52,7 @@ export default async function RepositoryView({ params }: RepositoryViewParams) {
           </div>
         </div>
 
-        <div className="flex justify-end gap-6 pb-6.5 sm:justify-center sm:pt-8">
+        <div className="flex justify-end gap-6 pt-6.5 pb-3 sm:justify-center sm:pt-8">
           <Link
             href="/"
             className="hover:bg-light/30 active:bg-light/30 size-10 rounded-md p-2 transition-normal duration-200 hover:scale-105 active:scale-105"
--- src/components/container/modal-container.tsx
+++ src/components/container/modal-container.tsx
@@ -10,7 +10,7 @@ export function ModalContainer({ children, ...props }: DialogHTMLAttributes<HTML
       className="bg-light dark:bg-dark text-dark dark:text-light animated-modal m-auto h-full max-h-[calc(100%-1.25rem)] w-full max-w-[calc(100%-1.25rem)] rounded shadow-md"
       {...props}
     >
-      <header className="text-light dark:bg-light/27 sticky top-0 -mb-10 flex bg-gradient-to-b from-emerald-600 px-4 pt-2.5 pb-10">
+      <header className="text-light sticky top-0 -mb-10 flex bg-gradient-to-b from-emerald-600 from-60% px-4 pt-2.5 pb-10">
         <ToggleModalButton action="close">
           <IoMdClose size={24} />
         </ToggleModalButton>
