f5ff8f163c7e5168dff76b583ed1de616f34c291
Ríad Oliveira <riad.oliveira@hotmail.com>
2025-05-07 20:03:06 -0300

    md designs for home and repository-card

--- src/components/container/header-container.tsx
+++ src/components/container/header-container.tsx
@@ -4,15 +4,16 @@ import { mergeStyles } from 'utils/mergeStyles';
 
 export function HeaderContainer() {
   return (
-    <header className="flex gap-5 pt-3 max-sm:justify-center max-sm:pb-6.5 sm:w-2/5 sm:flex-col sm:items-center sm:gap-3 sm:pt-4">
+    <header className="flex justify-center gap-5 max-sm:pt-3 max-sm:pb-6.5 sm:w-2/5 sm:flex-col sm:items-center sm:gap-3">
       <ImageContainer
-        className="ring-light/25 size-26 overflow-hidden rounded-2xl ring-2 sm:size-30 md:size-40 xl:size-48"
+        className="ring-light/25 size-26 overflow-hidden rounded-2xl ring-2 sm:size-30 md:size-[8.5rem] lg:size-40 xl:size-48"
         src="/avatar.jpg"
         alt={USER.fullName}
         sizes={{
           definitions: [
             { threshold: 'SM', value: '6.5rem' },
             { threshold: 'MD', value: '7.5rem' },
+            { threshold: 'LG', value: '8.5rem' },
             { threshold: 'XL', value: '10rem' },
           ],
           defaultValue: '12rem',
@@ -22,15 +23,15 @@ export function HeaderContainer() {
 
       <div className="flex flex-col py-1 max-sm:justify-between sm:gap-3">
         <section className="flex flex-col sm:items-center">
-          <h1 className="text-lg leading-none font-bold md:text-2xl">
+          <h1 className="text-lg leading-none font-bold md:text-xl lg:text-2xl">
             {USER.fullName}
           </h1>
-          <h2 className="font-secondary text-lg font-semibold md:text-[22px]">
+          <h2 className="font-secondary text-lg font-semibold md:text-xl lg:text-[22px]">
             Full Stack Developer
           </h2>
         </section>
 
-        <section className="flex flex-wrap max-md:justify-between md:max-w-[264px] md:justify-center md:gap-6">
+        <section className="flex flex-wrap max-sm:justify-between sm:w-full sm:gap-2.5 md:gap-3 lg:max-w-[264px] lg:justify-center lg:gap-6">
           {USER.socials.map(SocialLink)}
         </section>
       </div>
@@ -46,12 +47,12 @@ function SocialLink({ Icon, name, href, colorStyles }: SocialData) {
       target="_blank"
       rel="noopener noreferrer"
       className={mergeStyles([
-        'border-light/50 group flex size-9 flex-col items-center justify-center gap-2 rounded-md border-2 p-1 transition duration-400 hover:border-1 active:border-1 xl:size-30 xl:shadow-md',
+        'border-light/50 group flex size-9 flex-col items-center justify-center gap-2 rounded-md border-2 p-1 transition duration-400 hover:border-1 active:border-1 sm:size-9.5 md:size-11.5 xl:size-30 xl:shadow-md',
         colorStyles,
       ])}
     >
       <Icon className="size-full" />
-      <span className="text-sm font-semibold max-md:hidden">{name}</span>
+      <span className="text-sm font-semibold max-lg:hidden">{name}</span>
     </a>
   );
 }
--- src/components/main-title.tsx
+++ src/components/main-title.tsx
@@ -2,7 +2,7 @@ import { DefaultProps } from 'types/DefaultProps';
 
 export function MainTitle({ children }: DefaultProps) {
   return (
-    <h1 className="font-secondary py-3.5 text-[19px] font-semibold lg:py-4 lg:text-[32px]">
+    <h1 className="font-secondary py-3.5 text-[19px] font-semibold md:py-4 md:text-[23px] lg:text-[32px]">
       {children}
     </h1>
   );
