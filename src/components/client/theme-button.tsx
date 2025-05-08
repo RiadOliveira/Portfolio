ecacd2ad98bfdf0580723e82c63db23c068917af
Ríad Oliveira <riad.oliveira@hotmail.com>
2025-05-07 21:00:58 -0300

    md main title size reduced

--- src/components/client/theme-button.tsx
+++ src/components/client/theme-button.tsx
@@ -7,10 +7,10 @@ export function ThemeButton() {
     <button
       type="button"
       onClick={toggleTheme}
-      className="hover:bg-dark/85 dark:hover:bg-light/85 dark:hover:text-dark active:bg-dark/85 dark:active:bg-light/85 dark:active:text-dark relative size-9 rounded-full p-1 duration-300 sm:size-8.5 md:size-10 lg:size-12"
+      className="hover:bg-dark/85 dark:hover:bg-light/85 dark:hover:text-dark active:bg-dark/85 dark:active:bg-light/85 dark:active:text-dark relative size-9 rounded-full p-1 duration-300 sm:size-8.5 md:size-9.5 lg:size-12"
     >
-      <MdOutlineLightMode className="invisible absolute inset-0 m-auto size-7 opacity-0 transition-opacity duration-500 sm:size-6.5 md:size-8 lg:size-9 dark:visible dark:opacity-100" />
-      <MdOutlineDarkMode className="visible absolute inset-0 m-auto size-7 transition-opacity duration-500 sm:size-6.5 md:size-8 lg:size-9 dark:invisible dark:opacity-0" />
+      <MdOutlineLightMode className="invisible absolute inset-0 m-auto size-7 opacity-0 transition-opacity duration-500 sm:size-6.5 md:size-7.5 lg:size-9 dark:visible dark:opacity-100" />
+      <MdOutlineDarkMode className="visible absolute inset-0 m-auto size-7 transition-opacity duration-500 sm:size-6.5 md:size-7.5 lg:size-9 dark:invisible dark:opacity-0" />
     </button>
   );
 }
--- src/components/container/layout-container.tsx
+++ src/components/container/layout-container.tsx
@@ -8,7 +8,7 @@ export function LayoutContainer({ children }: DefaultProps) {
       <HeaderContainer />
 
       <main className="bg-light/27 dark:bg-dark/32.5 relative flex flex-1 flex-col overflow-y-hidden rounded-lg px-4 transition-colors duration-300 sm:max-w-[52%] lg:px-8">
-        <div className="border-light/80 absolute left-4 flex h-14 w-[calc(100%-2rem)] items-center justify-end border-b-2 pt-0.5 sm:h-12 md:h-16.5 lg:left-8 lg:h-20 lg:w-[calc(100%-4rem)] lg:border-b-[3px] lg:pt-1">
+        <div className="border-light/80 absolute left-4 flex h-14 w-[calc(100%-2rem)] items-center justify-end border-b-2 pt-0.5 sm:h-12 md:h-14 lg:left-8 lg:h-20 lg:w-[calc(100%-4rem)] lg:border-b-[3px] lg:pt-1">
           <ThemeButton />
         </div>
 
