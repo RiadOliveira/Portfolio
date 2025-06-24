b2b28981cc6d0ee309cad70adaa907f29d10d432
RiadOliveira <riad.oliveira@hotmail.com>
2025-06-24 01:43:34 -0300

    Modal Loader

--- src/app/[repository]/@modal/loading.tsx
+++ src/app/[repository]/@modal/loading.tsx
@@ -1,3 +1,7 @@
 export default function RepositoryModalLoading() {
-  return <div>Loading ...</div>;
+  return (
+    <div className="flex size-full items-center justify-center pb-32">
+      <div className="loader" />
+    </div>
+  );
 }
