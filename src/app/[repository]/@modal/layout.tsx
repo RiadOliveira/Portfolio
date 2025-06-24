8d0aa8622d57874e8d6c66ccd2369e2745806810
RiadOliveira <riad.oliveira@hotmail.com>
2025-06-24 02:05:56 -0300

    Modal style updates

--- src/app/[repository]/@modal/layout.tsx
+++ src/app/[repository]/@modal/layout.tsx
@@ -7,8 +7,8 @@ import { DefaultProps } from 'types/DefaultProps';
 export default function RepositoryModalLayout({ children }: DefaultProps) {
   return (
     <dialog id={MODAL_ID} className="animated-modal">
-      <div className="bg-light dark:bg-dark text-dark dark:text-light relative mx-auto mt-2 flex h-[calc(100%-1rem)] w-[calc(100%-1rem)] max-w-[700px] flex-col rounded-xl shadow-md ring-2 ring-emerald-400/70">
-        <header className="text-light z-10 flex max-h-[50px] min-h-[50px] w-full items-center justify-between rounded-t-lg bg-gradient-to-b from-emerald-600 to-emerald-400 px-3.5 shadow-lg shadow-emerald-600/80">
+      <div className="bg-light dark:bg-dark text-dark dark:text-light shadow-light/50 dark:shadow-dark/50 relative mx-auto mt-2.5 flex h-[calc(100%-1.25rem)] w-[calc(100%-1.25rem)] max-w-[700px] flex-col rounded-xl shadow">
+        <header className="text-light z-10 flex max-h-[50px] min-h-[50px] w-full items-center justify-between rounded-t-lg bg-gradient-to-b from-emerald-600 to-emerald-400 px-3.5 shadow-md shadow-emerald-600/80">
           <ToggleModalButton
             action="close"
             className="transition-normal duration-100 hover:scale-120 active:scale-120"
