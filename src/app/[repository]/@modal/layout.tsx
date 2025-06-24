81c867ae87e489650cc0e93b65ec968fd9474607
RiadOliveira <riad.oliveira@hotmail.com>
2025-06-24 03:03:36 -0300

    Modal scrollbar surpassing window fixed

--- src/app/[repository]/@modal/layout.tsx
+++ src/app/[repository]/@modal/layout.tsx
@@ -7,7 +7,7 @@ import { DefaultProps } from 'types/DefaultProps';
 export default function RepositoryModalLayout({ children }: DefaultProps) {
   return (
     <dialog id={MODAL_ID} className="animated-modal">
-      <div className="bg-light dark:bg-dark text-dark dark:text-light shadow-light/50 dark:shadow-dark/50 relative mx-auto mt-2.5 flex h-[calc(100%-1.25rem)] w-[calc(100%-1.25rem)] max-w-[700px] flex-col rounded-xl shadow">
+      <div className="bg-light dark:bg-dark text-dark dark:text-light shadow-light/50 dark:shadow-dark/50 relative mx-auto mt-2.5 flex h-[calc(100%-1.25rem)] w-[calc(100%-1.25rem)] max-w-[700px] flex-col overflow-y-hidden rounded-xl shadow">
         <header className="text-light z-10 flex max-h-[50px] min-h-[50px] w-full items-center justify-between rounded-t-lg bg-gradient-to-b from-emerald-600 to-emerald-400 px-3.5 shadow-md shadow-emerald-600/80">
           <ToggleModalButton
             action="close"
