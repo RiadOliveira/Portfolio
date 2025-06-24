13bd7bfe6cea0dff8ad17d00bf021d83defd55a8
RiadOliveira <riad.oliveira@hotmail.com>
2025-06-23 23:37:53 -0300

    Modal background animation fixed

--- src/components/container/modal-container.tsx
+++ src/components/container/modal-container.tsx
@@ -9,23 +9,21 @@ export function ModalContainer({
   ...props
 }: DialogHTMLAttributes<HTMLDialogElement>) {
   return (
-    <dialog
-      id={MODAL_ID}
-      className="bg-light dark:bg-dark text-dark dark:text-light animated-modal m-auto flex size-full max-h-[calc(100%-1rem)] max-w-[calc(100%-1rem)] flex-col overflow-y-hidden rounded shadow-md lg:max-w-[800px]"
-      {...props}
-    >
-      <header className="text-light z-10 flex max-h-[50px] min-h-[50px] w-full items-center justify-between bg-gradient-to-b from-emerald-600 to-emerald-400 px-3.5 shadow-lg shadow-emerald-600/60">
-        <ToggleModalButton
-          action="close"
-          className="transition-normal duration-100 hover:scale-120 active:scale-120"
-        >
-          <IoMdClose className="size-7 sm:size-6.5 md:size-7.5 lg:size-8" />
-        </ToggleModalButton>
+    <dialog id={MODAL_ID} {...props} className="animated-modal">
+      <div className="bg-light dark:bg-dark text-dark dark:text-light relative mx-auto mt-2 flex h-[calc(100%-1rem)] w-[calc(100%-1rem)] flex-col rounded-xl shadow-md ring-2 ring-emerald-400/70 lg:w-[700px]">
+        <header className="text-light z-10 flex max-h-[50px] min-h-[50px] w-full items-center justify-between rounded-t-lg bg-gradient-to-b from-emerald-600 to-emerald-400 px-3.5 shadow-lg shadow-emerald-600/80">
+          <ToggleModalButton
+            action="close"
+            className="transition-normal duration-100 hover:scale-120 active:scale-120"
+          >
+            <IoMdClose className="size-7 sm:size-6.5 md:size-7.5 lg:size-8" />
+          </ToggleModalButton>
 
-        <ThemeButton />
-      </header>
+          <ThemeButton />
+        </header>
 
-      {children}
+        {children}
+      </div>
     </dialog>
   );
 }
