d4542f54dd3c6d7b324e6f53401eaa8b083dc9c2
RiadOliveira <riad.oliveira@hotmail.com>
2025-07-03 01:53:34 -0300

    generateDisplayDataURL() URL updated

--- src/utils/generateDisplayDataURL.ts
+++ src/utils/generateDisplayDataURL.ts
@@ -2,5 +2,5 @@ import { GITHUB_URLS } from 'constants/requests';
 import { USER_DATA } from 'constants/userData';
 
 export function generateDisplayDataURL(repositoryName: string) {
-  return `${GITHUB_URLS.content}/${USER_DATA.login}/${repositoryName}/main/displayData.json`;
+  return `${GITHUB_URLS.content}/${USER_DATA.login}/${repositoryName}/main/.portfolio/displayData.json`;
 }
