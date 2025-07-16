9aebca8e7967e2a7698b5ca74720ad65891e2767
RiadOliveira <riad.oliveira@hotmail.com>
2025-07-16 13:37:40 -0300

    MainTitle increased on lg and bigger screens

--- src/components/main-title.tsx
+++ src/components/main-title.tsx
@@ -2,7 +2,7 @@ import { DefaultProps } from 'types/DefaultProps';
 
 export function MainTitle({ children }: DefaultProps) {
   return (
-    <h1 className="font-secondary my-3.5 text-[19px] font-semibold sm:my-2.5 md:my-3 md:text-[21px] lg:my-3.5 lg:text-[22px]">
+    <h1 className="font-secondary my-3.5 text-[19px] font-semibold sm:my-2.5 md:my-3 md:text-[21px] lg:my-3.5 lg:text-2xl">
       {children}
     </h1>
   );
